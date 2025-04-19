cc73ce4a4b6d5fbac4c6adfe3a05b12f273584e7
ananthi1611 <ananthipetchimuthu1611@gmail.com>
2025-04-19 15:13:33 +0530

    Fix: Correct function export

--- api/index.js
+++ api/index.js
@@ -1,11 +1,4 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-  res.send("Hello from Vercel Node.js App!");
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+module.exports = (req, res) => {
+    res.send("Hello from Node.js on Vercel!");
+  };
+  
